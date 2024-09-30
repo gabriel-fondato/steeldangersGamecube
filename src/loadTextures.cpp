77e7b26116201e6b43dd6297b5e0ce555e03e32e
Gabriel Fondato <gabrielfondato485@gmail.com>
2024-09-30 20:40:24 -0300

    now it does

--- src/loadTextures.cpp
+++ src/loadTextures.cpp
@@ -1,15 +1,15 @@
 #include <grrlib.h>
 #include "loadTextures.h" 
-
+#include "print.h"
 
 #include "atlasminecraft_bmp.h"
 
 GRRLIB_texImg *tilemap1 = nullptr;
 
 void loadTextures() {
-    
-    GRRLIB_texImg *tilemap1 = GRRLIB_LoadTexture(atlasminecraft_bmp);
+    tilemap1 = GRRLIB_LoadTexture(atlasminecraft_bmp);
     GRRLIB_InitTileSet(tilemap1,16,16,0);
+    console.print(100,100,RGBA(255,255,255,255),"textures loaded");
 }
 
 
